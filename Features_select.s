5cb4e05125187eebbe08445ed52a6cfbe866f48a
The_Learner <Bsantosh909@gmail.com>
2017-08-14 21:47:26 +0545

    added few more minor byte hacks

--- Features_select.s
+++ Features_select.s
@@ -2,15 +2,24 @@ FALSE EQU 0
 TRUE EQU 1
 
 // the lines below are where you will be manually setting TRUE or FALSE to either add or not to add the features
-// these are all the basic byte change fixes and minor hacks
 
-RUNNING_INDOOR 						EQU TRUE
-EV_CAP_TO_252 						EQU TRUE
-EGG_HATCH_LEVEL_1 					EQU TRUE
-POKEDEX_GLITCH_FIX 					EQU TRUE
-REMOVING_HELP_SYSTEM 				EQU TRUE
-ROAMING_POKEMON_IV_GLITCH_FIX 		EQU FALSE
+
+// these are all the basic byte change fixes and minor hacks
+// you wont need to set TRUE or FALSE in the C files for them
+
+RUNNING_INDOOR 							EQU TRUE
+EV_CAP_TO_252 							EQU TRUE
+EGG_HATCH_LEVEL_1 						EQU TRUE
+POKEDEX_GLITCH_FIX 						EQU TRUE
+REMOVING_HELP_SYSTEM 					EQU TRUE
+ROAMING_POKEMON_IV_GLITCH_FIX 			EQU TRUE
+POKE_NUMBER_WITHOUT_NATIONAL_DEX		EQU TRUE
+POKEMON_SEEN_COUNT						EQU TRUE
+NAMING_SCREEN_32x32_OW					EQU TRUE
+ROUGH_SKIN_UPDATE						EQU TRUE
+CRITICAL_HIT_UPDATE						EQU TRUE
+
 
 
 // these all the hacks below are some major hacks
-
+// you will have to set TRUE or FALSE in the C files as well else they wont work well
